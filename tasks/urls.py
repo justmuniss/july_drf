f34de20b232afd016ff30b806286f42426e704c9
Munisa <munisagafurova69@email.com>
2026-07-16 16:31:04 +0500

    11.07.2026

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list,task_detail
 
 
 urlpatterns = [
     path('tasks/', task_list, name='task_list'),
+    path('tasks/<int:pk>/',task_detail, name='task_detail'),
 ]
