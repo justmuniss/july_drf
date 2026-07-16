795a8403ab0810fe5ccb7fcc8abc8e48aa50cd29
Munisa <munisagafurova69@email.com>
2026-07-16 13:47:08 +0500

    04.07.2026

--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Task
+from .serializers import TaskSerializer
 
 
 @api_view(['GET'])
 def task_list(request):
-    return Response({
-        'message': "Salom API"})
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
